b5d178f2b75214fd0555e0c8835da6b7ced148f9
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-23 15:27:00 +0300

    add HW2 исправил ошибку

--- Home_Work_Lesson1/HW_2.py
+++ Home_Work_Lesson1/HW_2.py
@@ -7,6 +7,7 @@ print('x y z')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if (not (x or y or z)) == (not (x and y and z)):
-                print(x, y, z)
+            if (not (x or y or z)) == (not (x or y or z)):
+                print(x,y,z)
+
                 
