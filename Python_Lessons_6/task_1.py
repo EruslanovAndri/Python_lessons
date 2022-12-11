278a8e65448884a4ddc67812b1fed78e6a9ef725
EruslanovAndrei <eruslanov84as@gmail.com>
2022-12-11 10:07:39 +0300

    add task 1 lesson 6

---
+++ Python_Lessons_6/task_1.py
@@ -0,0 +1,55 @@
+# Task 1. Напишите пр-му вычисления арифмитичексого заданного строкой.
+# Используйтк операции +,-,/,*, приоретет операций стандартный.
+# 1 Находим знаки с наивысшим приоритетом / и *.Последование выполнения знаков с приоритетом.
+# 2 Калькулируем цифры с двух сторон знака.
+# 3 Находим знаки с наименьшим приоритетом и каклькулируем их
+# 4 Выводим результат
+
+# operation = input('Введите то, что будем считать => ').split()
+string = '10+200*3/2'
+lst = []
+buf = ''
+for i in range(len(string)):
+    if string[i].isdigit():
+        buf += string[i]
+    else:
+        lst.append(int(buf))
+        lst.append(string[i])
+        buf = ''
+else:
+    lst.append(int(buf))
+print(lst)
+
+while ('*' in lst) or ('/' in lst):
+    mult = -1
+    if '*' in lst:
+        mult = lst.index('*')
+
+    div = -1
+    if '/' in lst:
+        div = lst.index('/')
+        
+    if ((mult < div) and (mult != -1) and (div != -1)) or ((mult != -1) and (div == -1)):
+        res = lst[mult -1 ] * lst[mult + 1]
+        lst = lst[:mult - 1] + [res] + lst[mult + 2:]
+    elif (div < mult) and (div != -1) and (mult != -1) or ((div != -1) and (mult == -1)):
+        res = lst[div - 1] / lst[div + 1]
+        lst = lst[:div - 1] + [res] + lst[div + 2:]
+
+while ('+' in lst) or ('-' in lst):
+    plus = -1
+    if '+' in lst:
+        plus = lst.index('+')
+
+    minus = -1
+    if '-' in lst:
+        minus = lst.index('-')
+        
+    if ((plus < minus) and (plus != -1) and (minus != -1)) or ((plus != -1) and (minus == -1)):
+        res = lst[plus -1 ] + lst[plus + 1]
+        lst = lst[:plus - 1] + [res] + lst[plus + 2:]
+    elif (minus < plus) and (minus != -1) and (plus != -1) or ((minus != -1) and (plus == -1)):
+        res = lst[minus - 1] - lst[minus + 1]
+        lst = lst[:minus - 1] + [res] + lst[minus + 2:]
+print(lst)
+
